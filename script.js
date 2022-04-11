ae1e148661635c6a64665c8a6519d1650c82b105
Phacharapol Phukana <phacharapol18@gmail.com>
2022-04-11 12:58:37 -0700

    implimenting css, api, html

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-let random = Math.floor(Math.random()*752,919) + 1;
+let random = Math.floor(Math.random() * 752, 919) + 1;
+let btn = document.querySelector('#randomBtn');
 
-let movie = `https://api.themoviedb.org/3/movie/${random}?api_key=e279ef38d7322234f5dbce86698431bb&language=US`
 
-fetch(movie)
-.then(function (response){
-    console.log(response)
-    if(response.status === 404){
-        document.location.reload()
+// let newOpt =
+
+    function card() {
+        let random = Math.floor(Math.random() * 752, 919) + 1;
+        let movie = `https://api.themoviedb.org/3/movie/${random}?api_key=e279ef38d7322234f5dbce86698431bb&language=US`
+        fetch(movie)
+            .then(function (response) {
+                console.log(response)
+                if (response.status === 404) {
+                    document.location.reload()
+                }
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data)
+                document.querySelector('.poster').setAttribute('src', `https://image.tmdb.org/t/p/w500/${data.poster_path}`);
+
+            })
     }
-    return response.json()
-})
-.then(function (data){
-    console.log(data)
-    document.querySelector('.poster').setAttribute('src',`https://image.tmdb.org/t/p/w500/${data.poster_path}`);
-   
+
+function cards() {
+    let drinks = 'https://thecocktaildb.com/api/json/v1/1/random.php'
+    fetch(drinks)
+        .then(function (response) {
+            console.log(response)
+            if (response.status === 404) {
+                document.location.reload()
+            }
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data)
+            document.querySelector('.beverage').setAttribute('src', data.drinks[0].strDrinkThumb)
+        })
+}
+
+btn.addEventListener('click', function () {
+    cards()
+    card()
 })

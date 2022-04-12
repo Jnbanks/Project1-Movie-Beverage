bda0c25762f22449d3a1183db3fc21ba5e4dcfe6
Phacharapol Phukana <phacharapol18@gmail.com>
2022-04-12 09:27:19 -0700

    add ingredient

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let btn = document.querySelector('#randomBtn');
             .then(function (response) {
                 console.log(response)
                 if (response.status === 404) {
-                     document.location.reload()
+                    card()
               }
                 return response.json()
             })
@@ -28,7 +28,7 @@ function cards() {
         .then(function (response) {
             console.log(response)
             if (response.status === 404) {
-                document.location.reload()
+                cards()
             }
             return response.json()
         })
@@ -36,6 +36,81 @@ function cards() {
             console.log(data)
             document.querySelector('.beverage').setAttribute('src', data.drinks[0].strDrinkThumb)
             document.querySelector('.name-drink').textContent = data.drinks[0].strDrink;
+            if(data.drinks[0].strIngredient1 === null){
+                document.querySelector('li .ingredient1').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient1').textContent = data.drinks[0].strIngredient1
+            }
+            if(data.drinks[0].strIngredient2 === null){
+                document.querySelector('li .ingredient2').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient2').textContent = data.drinks[0].strIngredient2
+            }
+            if(data.drinks[0].strIngredient3 === null){
+                document.querySelector('li .ingredient3').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient3').textContent = data.drinks[0].strIngredient3
+            }
+            if(data.drinks[0].strIngredient4 === null){
+                document.querySelector('li .ingredient4').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient4').textContent = data.drinks[0].strIngredient4
+            }
+            if(data.drinks[0].strIngredient5 === null){
+                document.querySelector('li .ingredient5').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient5').textContent = data.drinks[0].strIngredient4
+            }
+            if(data.drinks[0].strIngredient6 === null){
+                document.querySelector('li .ingredient6').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient6').textContent = data.drinks[0].strIngredient6
+            }
+            if(data.drinks[0].strIngredient7 === null){
+                document.querySelector('li .ingredient7').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient7').textContent = data.drinks[0].strIngredient7
+            }
+            if(data.drinks[0].strIngredient8 === null){
+                document.querySelector('li .ingredient8').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient8').textContent = data.drinks[0].strIngredient8
+            }
+            if(data.drinks[0].strIngredient9 === null){
+                document.querySelector('li .ingredient9').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient9').textContent = data.drinks[0].strIngredient9
+            }
+            if(data.drinks[0].strIngredient10 === null){
+                document.querySelector('li .ingredient10').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient10').textContent = data.drinks[0].strIngredient10
+            }
+            if(data.drinks[0].strIngredient11 === null){
+                document.querySelector('li .ingredient11').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient11').textContent = data.drinks[0].strIngredient11
+            }
+            if(data.drinks[0].strIngredient12 === null){
+                document.querySelector('li .ingredient12').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient12').textContent = data.drinks[0].strIngredient12
+            }
+            if(data.drinks[0].strIngredient13 === null){
+                document.querySelector('li .ingredient13').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient13').textContent = data.drinks[0].strIngredient13
+            }
+            if(data.drinks[0].strIngredient14 === null){
+                document.querySelector('li .ingredient14').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient14').textContent = data.drinks[0].strIngredient14
+            }
+            if(data.drinks[0].strIngredient15 === null){
+                document.querySelector('li .ingredient15').style.display = 'none'
+            }else {
+                document.querySelector('li .ingredient15').textContent = data.drinks[0].strIngredient15
+            }
         })
 }
 
